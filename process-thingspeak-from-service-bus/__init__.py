4502044352012233268ae283fe151cb61ff01eea
mnbf9fca <robert.aleck@cynexia.com>
2020-04-12 10:44:19 +0100

    create template

---
+++ process-thingspeak-from-service-bus/__init__.py
@@ -0,0 +1,15 @@
+import logging
+
+import azure.functions as func
+import azure.functions.blob as b
+
+
+def main(message: func.ServiceBusMessage):
+    # Log the Service Bus Message as plaintext
+
+    message_content_type = message.content_type
+    message_body = message.get_body().decode("utf-8")
+
+    logging.info("Python ServiceBus topic trigger processed message.")
+    logging.info("Message Content Type: " + message_content_type)
+    logging.info("Message Body: " + message_body)
