d2177ca6622caff6601244485c186a34eaa68ff5
mnbf9fca <robert.aleck@cynexia.com>
2020-04-15 10:24:24 +0100

    create thingspeak sender

--- process-evohome-and-send-to-event-hub/__init__.py
+++ process-evohome-and-send-to-event-hub/__init__.py
@@ -3,13 +3,16 @@ import logging
 from os import getenv
 
 import azure.functions as func
-from azure.eventhub import EventData, EventHubProducerClient, EventDataBatch
+from azure.servicebus import ServiceBusClient, Message as SBC_message, TopicClient
+from azure.servicebus.control_client import ServiceBusService, Message as SBS_Message
+from json import dumps
 
 
 #pylint: disable=relative-beyond-top-level
 from ..EvoHome.EvohomeClient import EvohomeClient
 
 logger = logging.getLogger("azure.eventhub")
+
 
 def getenv_or_exception(var_name: str) -> str:
     """
@@ -34,47 +37,40 @@ def main(mytimer: func.TimerRequest) -> None:
     eh_usernamne = getenv_or_exception("evohome_username")
     eh_password = getenv_or_exception("evohome_password")
     eh_api_key = getenv_or_exception("evohome_api_key")
-    eh_connection_string = getenv_or_exception("eventhub_cs_send")
-    eh_name = getenv_or_exception("eventhub_name")
-    eh_partition_evohome = getenv("eventhub_partition_evohome") # None is ok, no need for exception
+    servicebus_namespace = getenv_or_exception("servicebus_namespace")
+    servicebus_shared_access_key_name = getenv_or_exception("servicebus_shared_access_key_name")
+    servicebus_shared_access_key_value = getenv_or_exception("servicebus_shared_access_key_value")
+    sb_topic_name = getenv_or_exception("servicebus_topic_name")
 
     # first, validate evohome connection
     ehc = EvohomeClient(username=eh_usernamne, password=eh_password, appid=eh_api_key)
     logging.info(f"Successfully authenticated to evohome API as {eh_usernamne}")
 
-    # now establish EventHubProducerClient
-    client = EventHubProducerClient.from_connection_string(
-        eh_connection_string,
-        eventhub_name=eh_name)
-    logging.info(f"Successfully create new EventHubProducerClient from eh_connection_string and eh_name: {client.get_eventhub_properties()}")
-
+    # now establish client
+    servicebus_client = ServiceBusClient(service_namespace=servicebus_namespace,
+                               shared_access_key_name=servicebus_shared_access_key_name,
+                               shared_access_key_value=servicebus_shared_access_key_value)
+    
+    logging.info(
+        f"Successfully create new ServiceBusClient with shared_access_key {servicebus_shared_access_key_name} to {servicebus_client.service_namespace}")
+    topic_client = servicebus_client.get_topic(sb_topic_name)
+    logging.info(f"Successfully created TopicClient for {topic_client.address }")
 
     # get all locations
     all_locs = ehc.get_all_locations()
     logging.info(f"found {len(all_locs)} locations")
     # now get all devices at all locations
-    all_devices_all_locs = (ehc.get_thermostat_temperatures(location["locationID"]) for location in all_locs)
+    all_devices_all_locs = (ehc.get_thermostat_temperatures(
+        location["locationID"]) for location in all_locs)
 
     # add to batches and send...
-    for device_list in all_devices_all_locs: add_to_batch_and_send(device_list, client, eh_partition_evohome)
+    for device_list in all_devices_all_locs:
+        add_to_batch_and_send(device_list, topic_client)
 
-def add_to_batch_and_send(device_list: dict, client: EventHubProducerClient, partition_id: str) -> None:
+
+def add_to_batch_and_send(device_list: dict, client: TopicClient) -> None:
     logging.info(f"processing batch for {len(device_list)} devices")
-    event_data_batch = client.create_batch(partition_id=partition_id)
-    for device in device_list:
-        try:
-            event_data_batch.add(EventData(device))
-        except ValueError:
-            # event_data_batch reached max size. Send and create new
-            send_batch(client, event_data_batch)
-            event_data_batch = client.create_batch(partition_id=partition_id)
-            event_data_batch.add(EventData(device))
-    # final batch
-    send_batch(client, event_data_batch)
+    
+    messages = [SBC_message(dumps(device).encode('utf-8')) for device in device_list]
+    client.send(messages=messages)
     logging.info("batch complete")
-
-def send_batch(client: EventHubProducerClient, event_data_batch: EventDataBatch) -> None:
-    logging.info(f"Sending batch of size {event_data_batch.size_in_bytes} - {event_data_batch._count} messages")
-    with client:
-        client.send_batch(event_data_batch)
-    logging.info("batch sent")
