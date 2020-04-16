7099baf2dfcd91128e26c66dede466c12e136bc1
mnbf9fca <robert.aleck@cynexia.com>
2020-04-16 12:06:45 +0100

    usign output binding rather than explicitly generating client

--- process-evohome-and-send-to-event-hub/__init__.py
+++ process-evohome-and-send-to-event-hub/__init__.py
@@ -22,7 +22,7 @@ def getenv_or_exception(var_name: str) -> str:
     return val
 
 
-def main(mytimer: func.TimerRequest) -> None:
+def main(mytimer: func.TimerRequest, outputSbMsg: func.Out[str]) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
@@ -30,9 +30,9 @@ def main(mytimer: func.TimerRequest) -> None:
         logger.info('The timer is past due!')
 
     logger.info('Python timer trigger function ran at %s', utc_timestamp)
-    process_evohome()
+    process_evohome(outputSbMsg: func.Out[str])
 
-def process_evohome() -> None:
+def process_evohome(outputSbMsg: func.Out[str]) -> None:
     
     # get config
     eh_usernamne = getenv_or_exception("evohome_username")
@@ -43,8 +43,8 @@ def process_evohome() -> None:
     sb_topic_name = getenv_or_exception("servicebus_evohome_topic")
 
     # create a topic client to send items to the servicebus topic
-    topic_client = TopicClient.from_connection_string(servicebus_evohome_conn_string, sb_topic_name)
-    logger.info(f"Successfully created TopicClient for {topic_client.address }")
+    # topic_client = TopicClient.from_connection_string(servicebus_evohome_conn_string, sb_topic_name)
+    # logger.info(f"Successfully created TopicClient for {topic_client.address }")
 
     # first, validate evohome connection
     ehc = EvohomeClient(username=eh_usernamne, password=eh_password, appid=eh_api_key)
@@ -59,11 +59,12 @@ def process_evohome() -> None:
 
     # add to batches and send...
     for device_list in all_devices_all_locs:
-        add_to_batch_and_send(device_list, topic_client)
+        add_to_batch_and_send(device_list, outputSbMsg)
 
-def add_to_batch_and_send(device_list: dict, client: TopicClient) -> None:
+def add_to_batch_and_send(device_list: dict, outputSbMsg: func.Out[str]) -> None:
     logger.info(f"processing batch for {len(device_list)} devices")
-    messages = [Message(dumps(device).encode('utf-8')) for device in device_list]
-    logger.info(f"{len(messages)} messages queued")
-    client.send(messages=messages)
+    # messages = [Message(dumps(device).encode('utf-8')) for device in device_list]
+    # logger.info(f"{len(messages)} messages queued")
+    # client.send(messages=messages)
+    outputSbMsg.set(dumps(device).encode('utf-8')) for device in device_list
     logger.info("batch complete")
