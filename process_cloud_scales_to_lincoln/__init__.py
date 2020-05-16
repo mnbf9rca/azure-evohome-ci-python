bbdd37a7bfdf3716c3fd2e8e3d9c7b8a1ed43136
mnbf9fca <robert.aleck@cynexia.com>
2020-05-16 16:02:45 +0100

    cloud_scales passthrough to lincoln

---
+++ process_cloud_scales_to_lincoln/__init__.py
@@ -0,0 +1,9 @@
+import logging
+
+import azure.functions as func
+
+
+def main(event: func.EventHubEvent):
+    logging.info('Python EventHub trigger processed an event: %s',
+                 event.get_body().decode('utf-8'))
+    return event.get_body().decode('utf-8')
